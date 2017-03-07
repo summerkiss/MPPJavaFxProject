d070a540541cbe504c529fd4d119c59354a4ba0b
summerkiss <yuyang@10.10.21.140>
2017-03-06 18:58:27 -0600

    second commit

--- src/edu/mum/cs/mpp/libarysys/view/LoginController.java
+++ src/edu/mum/cs/mpp/libarysys/view/LoginController.java
@@ -3,6 +3,7 @@ package edu.mum.cs.mpp.libarysys.view;
 import edu.mum.cs.mpp.libarysys.business.Staff;
 import edu.mum.cs.mpp.libarysys.dataaccess.DataAccess;
 import edu.mum.cs.mpp.libarysys.dataaccess.DataAccessFacade;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -23,14 +24,22 @@ public class LoginController {
 	@FXML
 	private Button btnLogin;
 	
+	@FXML
 	public void Login(ActionEvent event){
 		DataAccess da = new DataAccessFacade();
+		Application app = null;
+		
 		Staff staff = (Staff)da.readStaff(txtUsername.getText());
-		if(staff==null){
-			
-		}if(staff.getPassword().equals(txtPwd)){
-			lbInformation.setText("You are fool");
-			
+		if(staff==null||
+				!staff.getPassword().equals(txtPwd.getText())){
+//			System.out.println(staff.getId());
+//			System.out.println(staff.getPassword());
+//			System.out.println(txtPwd.getText());
+			lbInformation.setText("User name or password is not correct");			
+			lbInformation.visibleProperty().set(true);
+		}if(staff.getPassword().equals(txtPwd.getText())){
+			lbInformation.setText("You are fool");			
+			lbInformation.visibleProperty().set(true);
 		}
 		
 	}
