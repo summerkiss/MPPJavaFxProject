69b0c25713682f1a874a1693d32e4b88ed008905
YifengZhong <yf_zhong@hotmail.com>
2017-03-09 20:27:36 -0600

    merge fix

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,7 +22,6 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			AquaFx.style();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
